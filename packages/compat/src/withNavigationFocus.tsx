17f4d4adfda4fa9e3dc02e2c89f743b04cb7a5bc
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2023-08-24 14:39:11 +0800

    fix: restore navigation prop in withNavigationFocus

--- packages/compat/src/withNavigationFocus.tsx
+++ packages/compat/src/withNavigationFocus.tsx
@@ -1,27 +1,32 @@
 import * as React from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
-import { useIsFocused } from '@react-navigation/native';
+import { NavigationProp, ParamListBase, useIsFocused } from '@react-navigation/native';
+import type { CompatNavigationProp } from './types';
+import useCompatNavigation from './useCompatNavigation';
 
-type InjectedProps = {
+type InjectedProps<T extends NavigationProp<ParamListBase>> = {
   isFocused: boolean;
+  navigation: CompatNavigationProp<T>;
 };
 
 export default function withNavigationFocus<
-  P extends InjectedProps,
+  T extends NavigationProp<ParamListBase>,
+  P extends InjectedProps<T>,
   C extends React.ComponentType<P>
 >(Comp: C) {
   const WrappedComponent = ({
     onRef,
     ...rest
-  }: Exclude<P, InjectedProps> & {
+  }: Exclude<P, InjectedProps<T>> & {
     onRef?: C extends React.ComponentClass<any>
       ? React.Ref<InstanceType<C>>
       : never;
   }): React.ReactElement => {
     const isFocused = useIsFocused();
+    const navigation = useCompatNavigation<T>();
 
     // @ts-expect-error: type checking HOC is hard
-    return <Comp ref={onRef} isFocused={isFocused} {...rest} />;
+    return <Comp ref={onRef} isFocused={isFocused} navigation={navigation} {...rest} />;
   };
 
   WrappedComponent.displayName = `withNavigationFocus(${
