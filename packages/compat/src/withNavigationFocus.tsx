b23020147dca7f74dbb94b299f208cc81799597e
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2023-08-28 11:56:16 +0800

    fix: restore API specs from 4.x

--- packages/compat/src/withNavigationFocus.tsx
+++ packages/compat/src/withNavigationFocus.tsx
@@ -3,34 +3,52 @@ import * as React from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 import { NavigationProp, useIsFocused } from '@react-navigation/native';
 import useCompatNavigation from './useCompatNavigation';
-import type { NavigationParams, NavigationFocusInjectedProps } from './types';
+import type { NavigationParams } from './types';
 
 const withNavigation = <
-  C extends React.ComponentType<{}>,
+  C extends React.JSXElementConstructor<{}>,
   NP extends NavigationParams = NavigationParams
 >(
   Component: C
 ) => {
-  type WrappedP = React.ComponentProps<C> & NavigationFocusInjectedProps;
+  type P = React.ComponentProps<C>;
+  type WrappedP = P & {
+    /**
+     * **NOTE**: Overriden by `.onRef` prop. Do not use `.ref`.
+     *           As per official React Navigation 5 API, use `.onRef` instead!
+     */
+    ref?: never;
+    /**
+     * Forwards ref to the wrapped component.
+     */
+    onRef?: C extends React.ComponentClass<{}>
+      ? React.LegacyRef<InstanceType<C>>
+      : C extends React.ForwardRefExoticComponent<P>
+        ? React.RefAttributes<React.ElementRef<C>>['ref']
+        : never;
+  };
 
-  const componentDisplayName = Component.displayName || Component.name;
+  const componentDisplayName = (Component as React.ComponentType<WrappedP>).displayName || Component.name;
   const WrappedComponent = Component as unknown as React.ComponentType<WrappedP>;
   const wrappedComponentDisplayName = `withNavigation(${componentDisplayName})`;
 
-  const NavigationComponent = React.forwardRef<C, WrappedP>((props, ref) => {
+  const NavigationComponent = (props: WrappedP) => {
     const isFocused = useIsFocused();
     const navigation = useCompatNavigation<NavigationProp<NP>>();
 
     return (
     // @ts-expect-error: type checking HOC is hard
+    // if we hadn't override the built-in ref with our custom onRef (official API since v4!!!), this
+    // would've been fine without bypassing the type checking. unfortunately, v4 & v5/compat spec is
+    // like this, so, the bypass has to stay to ease the implementation...
       <WrappedComponent
         {...props}
-        ref={ref}
+        ref={props.onRef}
         isFocused={isFocused}
         navigation={navigation}
       />
     );
-  });
+  };
 
   hoistNonReactStatics(NavigationComponent, Component);
 
